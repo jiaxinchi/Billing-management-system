ee04a6a31750018b31a054647081425deeb41095
jiaxinchi <jiaxinchi@smail.nju.edu.cn>
2018-05-07 15:53:50 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+﻿from tkinter import *
+class Application(Frame):
+	def __init__(self,master=None):
+		Frame.__init__(self,master)
+		self.pack()
+		self.creatWidgets()
+	
+	def creatWidgets(self):
+		self.hellolabel = Label(self,text="hello world!")
+		self.hellolabel.pack()
+		self.quitbutton = Button(self, text='Quit',command=self.quit)
+		self.quitbutton.pack()
+		
+app = Application()
+# 设置窗口标题
+app.master.title('hello world')
+# 主消息循环
+app.mainloop()
+		
